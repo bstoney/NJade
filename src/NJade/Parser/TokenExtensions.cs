8759b42735f4e754fbf9d54ee59e756acaffb178
bstoney <bstoney@gmail.com>
2014-06-17 14:14:42 +0800

    Added TokenExtensions tests

--- src/NJade/Parser/TokenExtensions.cs
+++ src/NJade/Parser/TokenExtensions.cs
@@ -2,11 +2,13 @@ namespace NJade.Parser
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Monads;
     using System.Text;
 
     using NJade.Lexer;
     using NJade.Lexer.Tokenizer;
+    using NJade.Lexer.Tokenizer.Strings;
     using NJade.Parser.Elements;
 
     /// <summary>
@@ -15,7 +17,7 @@ namespace NJade.Parser
     internal static class TokenExtensions
     {
         /// <summary>
-        /// Raises the unexpected token.
+        /// Raises an unexpected token exception.
         /// </summary>
         /// <param name="tokens">The tokens.</param>
         public static void RaiseUnexpectedToken(this TokenStream tokens)
@@ -30,11 +32,16 @@ namespace NJade.Parser
                 throw new NJadeException("Invalid token.");
             }
 
-            throw new NJadeException(string.Format("Unexpected {0} at line: {1}, column: {2}.", tokens.Current.TokenValue, tokens.Current.Line, tokens.Current.Column));
+            throw new NJadeException(
+                string.Format(
+                    "Unexpected {0} at line: {1}, column: {2}.",
+                    tokens.Current.TokenValue,
+                    tokens.Current.Line,
+                    tokens.Current.Column));
         }
 
         /// <summary>
-        /// Gets the current token.
+        /// Gets the value of the current token.
         /// </summary>
         /// <param name="tokens">The tokens.</param>
         /// <returns>The value of the current token.</returns>
@@ -45,17 +52,22 @@ namespace NJade.Parser
                 return null;
             }
 
+            if (tokens.Current == null)
+            {
+                throw new NJadeException("Invalid token.");
+            }
+
             var currentValue = tokens.Current.TokenValue;
             tokens.Consume();
             return currentValue;
         }
 
         /// <summary>
-        /// Gets the current token .
+        /// Gets the value of the current token if it matches the token type.
         /// </summary>
         /// <param name="tokens">The tokens.</param>
-        /// <param name="tokenType">Type of the token.</param>
-        /// <returns>The value of the current token.</returns>
+        /// <param name="tokenType">Type of the Tokens.</param>
+        /// <returns>The value of the current Tokens.</returns>
         public static string Get(this TokenStream tokens, TokenType tokenType)
         {
             if (!tokens.Is(tokenType))
@@ -79,7 +91,11 @@ namespace NJade.Parser
                 sb.Append(tokens.Get());
             }
 
-            tokens.Consume();
+            if (!tokens.IsAtEnd())
+            {
+                tokens.Consume();
+            }
+
             return sb.ToString();
         }
 
@@ -101,14 +117,14 @@ namespace NJade.Parser
         }
 
         /// <summary>
-        /// Determines whether the current token is any of the supplied token types.
+        /// Determines whether the current tokent is any of the supplied token types.
         /// </summary>
         /// <param name="tokens">The tokens.</param>
         /// <param name="tokenTypes">The token types.</param>
         /// <returns>true if the current token matches; otherwise, false.</returns>
         public static bool IsAny(this TokenStream tokens, params TokenType[] tokenTypes)
         {
-            return tokenTypes.Contains(tokens.Current.TokenType);
+            return !tokens.IsAtEnd() && tokens.Current.With(t => tokenTypes.Contains(t.TokenType));
         }
 
         /// <summary>
@@ -119,19 +135,40 @@ namespace NJade.Parser
         /// <returns>true if the current token matches; otherwise, false.</returns>
         public static bool Is(this TokenStream tokens, TokenType tokenType)
         {
-            return tokens.Current.With(t => t.TokenType == tokenType);
+            return !tokens.IsAtEnd() && tokens.Current.With(t => t.TokenType == tokenType);
         }
 
         /// <summary>
-        /// Gets the elements.
+        /// Peeks at the remaining tokens.
+        /// </summary>
+        /// <param name="tokens">The tokens.</param>
+        /// <returns>A list of tokens.</returns>
+        public static List<StringToken> PeekAtRemainingTokens(this TokenStream tokens)
+        {
+            var tokenList = new List<StringToken>();
+            tokens.TakeSnapshot();
+
+            while (!tokens.IsAtEnd())
+            {
+                tokenList.Add(tokens.Current);
+                tokens.Consume();
+            }
+
+            tokens.RollbackSnapshot();
+            return tokenList;
+        }
+
+        /// <summary>
+        /// Gets all the elements where the intent is greated than the indent supplied.
         /// </summary>
         /// <param name="tokens">The tokens.</param>
         /// <param name="indent">The indent.</param>
-        /// <returns>A list od jade elements.</returns>
+        /// <returns>A list of jade elements.</returns>
         public static List<JElement> GetItems(this TokenStream tokens, int? indent)
         {
             var items = new List<JElement>();
-            while (!tokens.IsAtEnd() && (indent == null || (tokens.Is(TokenType.WhiteSpace) && tokens.Current.TokenValue.Length > indent)))
+            while (!tokens.IsAtEnd()
+                   && (indent == null || (tokens.Is(TokenType.WhiteSpace) && tokens.Current.TokenValue.Length > indent)))
             {
                 var thisIndent = 0;
                 if (tokens.Current.TokenType == TokenType.WhiteSpace)
@@ -162,5 +199,71 @@ namespace NJade.Parser
 
             return items;
         }
+
+        ////    /// <summary>
+        ////    /// Gets the child tokens.
+        ////    /// </summary>
+        ////    /// <param name="tokens">The tokens.</param>
+        ////    /// <param name="indent">The indent.</param>
+        ////    /// <returns>A new Tokens stream.</returns>
+        ////    public static TokenStream GetChildTokens(this TokenStream tokens, int? indent)
+        ////    {
+        ////        var tokenList = new List<StringToken>();
+        ////        while (!tokens.IsAtEnd() && (indent == null || (tokens.Is(TokenType.WhiteSpace) && tokens.Current.TokenValue.Length > indent)))
+        ////        {
+        ////            tokenList.Add(tokens.Current);
+        ////        }
+
+        ////        return new TokenStream(tokenList);
+        ////    }
+
+        ////    public static IEnumerable<TokenHierarchy> GetTokenHierarchy(this TokenStream tokens, int? indent)
+        ////    {
+        ////        while (!tokens.IsAtEnd())
+        ////        {
+        ////            var current = tokens.Current;
+        ////            var children = tokens.GetChildTokens(indent ?? 0).GetTokenHierarchy(indent ?? 0);
+
+        ////            yield return new TokenHierarchy(current, children);
+        ////        }
+        ////    }
+        ////}
+
+        /////// <summary>
+        /////// Defines the TokenHierarchy class.
+        /////// </summary>
+        ////internal class TokenHierarchy
+        ////{
+        ////    /// <summary>
+        ////    /// Initializes a new instance of the <see cref="TokenHierarchy"/> class.
+        ////    /// </summary>
+        ////    /// <param name="tokens">The Tokens.</param>
+        ////    /// <param name="children">The children.</param>
+        ////    public TokenHierarchy(IEnumerable<StringToken> tokens, IEnumerable<TokenHierarchy> children)
+        ////    {
+        ////        this.Tokens = new TokenStream(tokens);
+        ////        this.Children = children;
+        ////    }
+
+        ////    /// <summary>
+        ////    /// Gets the Tokens.
+        ////    /// </summary>
+        ////    /// <value>
+        ////    /// The Tokens.
+        ////    /// </value>
+        ////    public TokenStream Tokens { get; private set; }
+
+        ////    /// <summary>
+        ////    /// Gets or sets the children.
+        ////    /// </summary>
+        ////    /// <value>
+        ////    /// The children.
+        ////    /// </value>
+        ////    public IEnumerable<TokenHierarchy> Children { get; private set; }
+        ////}
+
+        ////internal class JElementCollection
+        ////{
+        ////}
     }
 }
--- test/NJadeTestSuite/Parser/TokenExtensionsTests.cs
+++ test/NJadeTestSuite/Parser/TokenExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace NJadeTestSuite.Parser
 {
+    using System.Collections.Generic;
     using System.Linq;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,6 +8,7 @@
     using Moq;
 
     using NJade;
+    using NJade.Lexer;
     using NJade.Lexer.Tokenizer;
     using NJade.Lexer.Tokenizer.Strings;
     using NJade.Parser;
@@ -31,5 +33,272 @@
             tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
             tokens.Object.RaiseUnexpectedToken();
         }
+
+        /// <summary>
+        /// Tests that the token extensions should raise an unexpected end of template exception when raising an unexpcected token exception and the token stream is empty.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Unexpected end of template.")]
+        public void ShouldRaiseAnUnexpectedEndOfTemplateExceptionWhenRaisingAnUnexpcectedTokenExceptionAndTheTokenStreamIsEmpty()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(true);
+            tokens.Object.RaiseUnexpectedToken();
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should raise an invalid token exception when raising an unexpcected token exception and the token is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Invalid token.")]
+        public void ShouldRaiseAnInvalidTokenExceptionWhenRaisingAnUnexpcectedTokenExceptionAndTheTokenIsNull()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(false);
+            tokens.Setup(t => t.Current).Returns((StringToken)null);
+            tokens.Object.RaiseUnexpectedToken();
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should get the next token value.
+        /// </summary>
+        [TestMethod]
+        public void ShouldGetTheNextTokenValue()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(false);
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.AreEqual("hello", tokens.Object.Get());
+            tokens.Verify(t => t.Consume(), Times.Once);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return null when getting the next token value and the stream is empty.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnNullWhenGettingTheNextTokenValueAndTheStreamIsEmpty()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(true);
+            Assert.IsNull(tokens.Object.Get());
+            tokens.Verify(t => t.Consume(), Times.Never);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should raise an invalid token exception when trying to get the next token value and the token is null.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Invalid token.")]
+        public void ShouldRaiseAnInvalidTokenExceptionWhenTryingToGetTheNextTokenValueAndTheTokenIsNull()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(false);
+            tokens.Setup(t => t.Current).Returns((StringToken)null);
+            tokens.Object.Get();
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should get the next token value when the token type is specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldGetTheNextTokenValueWhenTheTokenTypeIsSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(false);
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.AreEqual("hello", tokens.Object.Get(TokenType.Word));
+            tokens.Verify(t => t.Consume(), Times.Once);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should raise an invalid token exception when trying to get the next token and the token does not match the token type specified.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Unexpected hello at line: 1, column: 1.")]
+        public void ShouldRaiseAnUnexpectedTokenExceptionWhenTryingToGetTheNextTokenAndTheTokenDoesNotMatchTheTokenTypeSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(false);
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            tokens.Object.Get(TokenType.QuotedString);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should get every token until a new line is reached.
+        /// </summary>
+        [TestMethod]
+        public void ShouldGetEveryTokenUntilANewLineIsReached()
+        {
+            var tokens = new Mock<TokenStream>((IEnumerable<StringToken>)new[]
+                                                   {
+                                                       new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                       new StringToken(TokenType.WhiteSpace, 1, 1, 1, "hello"),
+                                                       new StringToken(JadeTokenType.NewLine, 1, 1, 1, "hello")
+                                                   });
+            tokens.CallBase = true;
+            Assert.AreEqual("hellohello", tokens.Object.GetLine());
+            tokens.Verify(t => t.Consume(), Times.Exactly(3));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should get every token until the stream is empty.
+        /// </summary>
+        [TestMethod]
+        public void ShouldGetEveryTokenUntilTheStreamIsEmpty()
+        {
+            var tokens = new Mock<TokenStream>((IEnumerable<StringToken>)new[]
+                                                   {
+                                                       new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                       new StringToken(TokenType.WhiteSpace, 1, 1, 1, "hello"),
+                                                   });
+            tokens.CallBase = true;
+            Assert.AreEqual("hellohello", tokens.Object.GetLine());
+            tokens.Verify(t => t.Consume(), Times.Exactly(2));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should call consume when the token is any of the token types specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldCallConsumeWhenTheTokenIsAnyOfTheTokenTypesSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            tokens.Object.ConsumeAny(TokenType.WhiteSpace, TokenType.Word);
+            tokens.Verify(t => t.Consume(), Times.Once);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should raise an unexpected token exception when the token is not any of the token types specified.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Unexpected hello at line: 1, column: 1.")]
+        public void ShouldRaiseAnUnexpectedTokenExceptionWhenTheTokenIsNotAnyOfTheTokenTypesSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            tokens.Object.ConsumeAny(TokenType.WhiteSpace, TokenType.QuotedString);
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return true when the token is any of the token types specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnTrueWhenTheTokenIsAnyOfTheTokenTypesSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.IsTrue(tokens.Object.IsAny(TokenType.WhiteSpace, TokenType.Word));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when the token is not any of the token types specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTheTokenIsNotAnyOfTheTokenTypesSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.IsFalse(tokens.Object.IsAny(TokenType.WhiteSpace, TokenType.QuotedString));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when trying to match any of the supplied token types and the token is null.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTryingToMatchAnyOfTheSuppliedTokenTypesAndTheTokenIsNull()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns((StringToken)null);
+            Assert.IsFalse(tokens.Object.IsAny(TokenType.WhiteSpace, TokenType.QuotedString));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when trying to match any of the supplied token types and the stream is empty.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTryingToMatchAnyOfTheSuppliedTokenTypesAndTheStreamIsEmpty()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(true);
+            Assert.IsFalse(tokens.Object.IsAny(TokenType.WhiteSpace, TokenType.QuotedString));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return true when the token is the token type specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnTrueWhenTheTokenIsTheTokenTypeSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.IsTrue(tokens.Object.Is(TokenType.Word));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when the token is not the token type specified.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTheTokenIsNotTheTokenTypeSpecified()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.Word, 1, 1, 1, "hello"));
+            Assert.IsFalse(tokens.Object.Is(TokenType.WhiteSpace));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when trying to match the supplied token types and the token is null.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTryingToMatchTheSuppliedTokenTypesAndTheTokenIsNull()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.Current).Returns((StringToken)null);
+            Assert.IsFalse(tokens.Object.Is(TokenType.WhiteSpace));
+        }
+
+        /// <summary>
+        /// Tests that the token extensions should return false when trying to match the supplied token types and the stream is empty.
+        /// </summary>
+        [TestMethod]
+        public void ShouldReturnFalseWhenTryingToMatchTheSuppliedTokenTypesAndTheStreamIsEmpty()
+        {
+            var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+            tokens.Setup(t => t.IsAtEnd()).Returns(true);
+            Assert.IsFalse(tokens.Object.Is(TokenType.WhiteSpace));
+        }
+
+        /////// <summary>
+        /////// Tests that the token extensions should return an empty list when getting child tokens and the stream is empty.
+        /////// </summary>
+        ////[TestMethod]
+        ////public void ShouldReturnAnEmptyListWhenGettingChildTokensAndTheStreamIsEmpty()
+        ////{
+        ////    var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
+        ////    tokens.Setup(t => t.IsAtEnd()).Returns(true);
+        ////    var tokenHierarchy = tokens.Object.GetTokenHierarchy(null);
+        ////    Assert.IsNotNull(tokenHierarchy);
+        ////    Assert.AreEqual(0, tokenHierarchy.Count());
+        ////}
+
+        /////// <summary>
+        /////// Tests that the token extensions should return all tokens on a line when getting child tokens and the indent doesnt change.
+        /////// </summary>
+        ////[TestMethod]
+        ////public void ShouldReturnAllTokensOnALineWhenGettingChildTokensAndTheIndentDoesntChange()
+        ////{
+        ////    var stringTokens = new[]
+        ////                           {
+        ////                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+        ////                               new StringToken(TokenType.WhiteSpace, 1, 1, 1, "hello"),
+        ////                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+        ////                               new StringToken(JadeTokenType.NewLine, 1, 1, 1, "hello"),
+        ////                           };
+        ////    var tokens = new Mock<TokenStream>((IEnumerable<StringToken>)stringTokens);
+        ////    tokens.CallBase = true;
+        ////    var tokenHierarchy = tokens.Object.GetTokenHierarchy(null).ToArray();
+        ////    Assert.AreEqual(1, tokenHierarchy.Length);
+        ////    CollectionAssert.AreEqual(stringTokens, tokenHierarchy[0].Tokens);
+        ////}
     }
 }
