1450d36cd3718e8492954167564eb071deeed53f
bstoney <bstoney@gmail.com>
2014-06-25 08:05:04 +0800

    Added more TagPaser tests

--- test/NJadeTestSuite/Parser/Parsers/TagParserTests.cs
+++ test/NJadeTestSuite/Parser/Parsers/TagParserTests.cs
@@ -17,6 +17,9 @@
 
     using NJadeTestSuite.TestHelper;
 
+    /// <summary>
+    /// Defines the TagParserTests class.
+    /// </summary>
     [TestClass]
     public class TagParserTests
     {
@@ -60,10 +63,10 @@
         }
 
         /// <summary>
-        /// Tests that the tag parser should return false when calling can parse andhe next token is not a dot, hash or word.
+        /// Tests that the tag parser should return false when calling can parse and the next token is not a dot, hash or word.
         /// </summary>
         [TestMethod]
-        public void ShouldReturnFalseWhenCallingCanParseAndheNextTokenIsNotADotHashOrWord()
+        public void ShouldReturnFalseWhenCallingCanParseAndTheNextTokenIsNotADotHashOrWord()
         {
             var tokens = new Mock<TokenStream>(Enumerable.Empty<StringToken>());
             tokens.Setup(t => t.Current).Returns(new StringToken(TokenType.QuotedString, 1, 1, 1));
@@ -145,10 +148,10 @@
         }
 
         /// <summary>
-        /// Tests that the tag parser should parse a class name when calling parse andd the first token is a dot followed by a class name.
+        /// Tests that the tag parser should parse a class name when calling parse and the first token is a dot followed by a class name.
         /// </summary>
         [TestMethod]
-        public void ShouldParseAClassNameWhenCallingParseAnddTheFirstTokenIsADotFollowedByAWord()
+        public void ShouldParseAClassNameWhenCallingParseAndTheFirstTokenIsADotFollowedByAWord()
         {
             var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
                                                                            {
@@ -203,10 +206,10 @@
         }
 
         /// <summary>
-        /// Tests that the tag parser should parse an identifier when calling parse andd the first token is a hash followed by a word.
+        /// Tests that the tag parser should parse an identifier when calling parse and the first token is a hash followed by a word.
         /// </summary>
         [TestMethod]
-        public void ShouldParseAnIdWhenCallingParseAnddTheFirstTokenIsAHashFollowedByAWord()
+        public void ShouldParseAnIdWhenCallingParseAndTheFirstTokenIsAHashFollowedByAWord()
         {
             var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
                                                                            {
@@ -254,6 +257,158 @@
                                                                                new StringToken(TokenType.Word, 1, 1, 1, "hello"),
                                                                                new StringToken(JadeTokenType.Hash, 1, 1, 1, "hash"),
                                                                                new StringToken(TokenType.Word, 1, 1, 1, "hello")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            tagParser.Parse(tokens.Object);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should parse an identifier and class names when calling parse and an identifier and class name has been supplied.
+        /// </summary>
+        [TestMethod]
+        public void ShouldParseAnIdAndClassNameWhenCallingParseAndAnIdAndClassNameHasBeenSupplied()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(JadeTokenType.Hash, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.Dot, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            var tag = tagParser.Parse(tokens.Object) as JTag;
+
+            Assert.AreEqual("hello", tag.Id);
+            CollectionAssert.AreEqual(new[] { "hello" }, tag.Classes);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should parse an identifier and class names when calling parse and the identifier and class names are in any order.
+        /// </summary>
+        [TestMethod]
+        public void ShouldParseAnIdAndClassNamesWhenCallingParseAndTheIdAndClassNamesAreInAnyOrder()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(JadeTokenType.Dot, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.Hash, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.Dot, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            var tag = tagParser.Parse(tokens.Object) as JTag;
+
+            Assert.AreEqual("hello", tag.Id);
+            CollectionAssert.AreEqual(new[] { "hello", "hello" }, tag.Classes);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should parse an identifier when calling parse and an identifier follows the tag.
+        /// </summary>
+        [TestMethod]
+        public void ShouldParseAnAttributeWhenCallingParseAndAttributesFollowTheTag()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.OpenParenth, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.Equals, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.QuotedString, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.CloseParenth, 1, 1, 1, "hello")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            var tag = tagParser.Parse(tokens.Object) as JTag;
+
+            Assert.AreEqual(1, tag.Attributes.Count);
+            Assert.IsInstanceOfType(tag.Attributes[0], typeof(JAttribute));
+
+            var attribute = tag.Attributes[0] as JAttribute;
+            Assert.AreEqual("hello", attribute.Name);
+            Assert.AreEqual("hello", attribute.Value);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should not parse any attribute when calling parse and parentheses are empty.
+        /// </summary>
+        [TestMethod]
+        public void ShouldNotParseAnyAttributeWhenCallingParseAndParenthesesAreEmpty()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.OpenParenth, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.CloseParenth, 1, 1, 1, "hello"),
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            var tag = tagParser.Parse(tokens.Object) as JTag;
+
+            Assert.AreEqual(0, tag.Attributes.Count);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should not parse any attribute when calling parse and no attributes follow the tag.
+        /// </summary>
+        [TestMethod]
+        public void ShouldNotParseAnyAttributeWhenCallingParseAndNoAttributesFollowTheTag()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            var tag = tagParser.Parse(tokens.Object) as JTag;
+
+            Assert.AreEqual(0, tag.Attributes.Count);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should throw an unexpected token exception when calling parse and an open parentheses exists and a close parentheses does not.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Unexpected newline at line: 1, column: 1.")]
+        public void ShouldThrowAnUnexpectedTokenExceptionWhenCallingParseAndAnOpenParenthesesExistsAndACloseParenthesesDoesNot()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.OpenParenth, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.NewLine, 1, 1, 1, "newline")
+                                                                           });
+            tokens.CallBase = true;
+
+            var tagParser = new TagParser();
+            tagParser.Parse(tokens.Object);
+        }
+
+        /// <summary>
+        /// Tests that the tag parser should throw an unexpected token exception when calling parse and a value is missing from an attribute.
+        /// </summary>
+        [TestMethod]
+        [ExpectedExceptionMessage(typeof(NJadeException), "Unexpected parenth at line: 1, column: 1.")]
+        public void ShouldThrowAnUnexpectedTokenExceptionWhenCallingParseAndAValueIsMissingFromAnAttribute()
+        {
+            var tokens = new Mock<TokenLine>((IEnumerable<StringToken>)new[]
+                                                                           {
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.OpenParenth, 1, 1, 1, "hello"),
+                                                                               new StringToken(TokenType.Word, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.Equals, 1, 1, 1, "hello"),
+                                                                               new StringToken(JadeTokenType.CloseParenth, 1, 1, 1, "parenth")
                                                                            });
             tokens.CallBase = true;
 
